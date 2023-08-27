8a1a9d1b1a52d5f32b694bfacc6144eb0b10448c
romuloabbiati <rhabbiati@hotmail.com>
2023-08-27 09:06:52 +0100

    navbar with burger button

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,13 +1,27 @@
 import './styles.css';
+import '@popperjs/core';
+import 'bootstrap/js/src/collapse';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-md navbar-light bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#bdsdesafio-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggle-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id='bdsdesafio-navbar'>
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
               <a href="link" className="active">
