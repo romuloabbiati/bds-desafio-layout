ba88f391446b5aec1ac03b1f8116fafa875ab027
romuloabbiati <rhabbiati@hotmail.com>
2023-09-03 09:05:44 +0100

    product card created

---
+++ frontweb/src/components/ProductCard/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+
+import ProductImg from 'assets/images/product.png';
+import ButtonIcon from 'components/ButtonIcon';
+
+const ProductCard = () => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img src={ProductImg} alt="Nome do produto" />
+      </div>
+      <div className="card-bottom-container">
+        <h6>Audi Supra TT</h6>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
+          nisi
+        </p>
+        <ButtonIcon />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
